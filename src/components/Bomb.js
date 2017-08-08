87641e1124bcd6948072a254fa7a4313d124bb23
cheungth25 <cheungth25@gmail.com>
2017-08-08 13:29:15 -0400

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,26 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+
+export default class Bomb extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  };
+
+  render(){
+    return(
+      <p>{didIBlowYet(this.state.secondsLeft)}</p>
+    )
+  }
+};
+
+function didIBlowYet(tickTock){
+  if (tickTock>0){
+    return `${tickTock} seconds left before I go boom!`
+  }else if (tickTock===0){
+    return "Boom!"
+  }
+};
